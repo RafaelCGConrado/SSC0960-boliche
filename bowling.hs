c2842501a161329c69db6509a8ae5ebe0c841cc1
Victor Hugo Amaral <amaral.vht@gmail.com>
2023-05-24 18:44:58 -0300

    Contagem funcionando

--- bowling.hs
+++ bowling.hs
@@ -1,16 +1,31 @@
-somaPontos [] = 0 
-somaPontos (h:t) = do
-  -- |h == 10 = h + head t + head $ tail t
-  |h == 10 = h + somaPontos t
+somaBonusStrike :: [Int] -> Int
+somaBonusStrike [] = 0
+somaBonusStrike (cab:cau) = cab + head cau
+
+somaBonusSpare :: [Int] -> Int
+somaBonusSpare [] = 0
+somaBonusSpare (cab:cau) = cab
+
+somaPontos :: Int -> [Int] -> Int
+somaPontos pontos [] = pontos + 0
+somaPontos pontos (cab:cau)
+  | cab == 10 = cab + somaBonusStrike cau + somaPontos pontos cau
+  | cab + head cau == 10 = cab + head cau + somaBonusSpare (tail cau) + somaPontos pontos (tail cau)
+  | otherwise = cab + head cau + somaPontos pontos (tail cau)
   
--- soma (h:t) = do
---  | h 10 = asihuifh(soma)
---  | h != 10 ouihaiuhc(soma)
 
 main :: IO ()
 main = do
     putStrLn "Digite o placar de pontos: "
     line <- getLine
-    let numbers = map read (words line) :: [Integer]
-    putStrLn $ "Primeiro elemento: " ++ show (head (tail numbers))
+    let numbers = map read (words line)
 
+    let pontos = 0
+
+    putStrLn $ "Pontuacao: " ++ show (somaPontos pontos numbers)
+
+
+
+
+
+    
