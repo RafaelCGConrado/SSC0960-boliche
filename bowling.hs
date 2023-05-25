ceefa10a56ebb1d4010594a195e9b80712ff2525
Victor Hugo Amaral <amaral.vht@gmail.com>
2023-05-24 21:04:21 -0300

    concluido

--- bowling.hs
+++ bowling.hs
@@ -1,31 +1,66 @@
+cabeca :: [Int] -> Int
+cabeca [] = 0
+cabeca pinos = head pinos
+
+cauda :: [Int] -> [Int]
+cauda [] = []
+cauda pinos = tail pinos
+
 somaBonusStrike :: [Int] -> Int
 somaBonusStrike [] = 0
-somaBonusStrike (cab:cau) = cab + head cau
+somaBonusStrike (cab:cau)
+  | cauda cau == [] = 0
+  | otherwise = cab + cabeca cau
 
 somaBonusSpare :: [Int] -> Int
 somaBonusSpare [] = 0
-somaBonusSpare (cab:cau) = cab
+somaBonusSpare (cab:cau)
+  | cau == [] = 0 
+  | cauda cau == [] = cab
+  | otherwise = cab
 
-somaPontos :: Int -> [Int] -> Int
-somaPontos pontos [] = pontos + 0
-somaPontos pontos (cab:cau)
-  | cab == 10 = cab + somaBonusStrike cau + somaPontos pontos cau
-  | cab + head cau == 10 = cab + head cau + somaBonusSpare (tail cau) + somaPontos pontos (tail cau)
-  | otherwise = cab + head cau + somaPontos pontos (tail cau)
-  
+somaPontos :: [Int] -> Int
+somaPontos [] = 0
+somaPontos (cab:cau)
+  | cab == 10 = cab + somaBonusStrike cau + somaPontos cau
+  | cab + cabeca cau == 10 = cab + cabeca cau + somaBonusSpare (cauda cau) + somaPontos (cauda cau)
+  | otherwise = cab + cabeca cau + somaPontos (cauda cau)
+
+
+printPontos :: [Int] -> IO ()
+printPontos [] = return ()  -- Caso base: lista vazia, não faz nada
+printPontos (cab:cau)
+  | cab == 10 = do -- Strike
+    if cauda (cauda cau) == [] then do
+      putStr "X "
+      if cau /= [] then
+        printPontos cau
+      else putStr ""   
+    else do 
+      putStr "X _ | " 
+      printPontos cau
+      
+  | cab + cabeca cau == 10 = do -- Spare
+    if cauda (cauda cau) == [] then putStr $ show (cab) ++ " / "
+    else putStr $ show (cab) ++ " / | "
+    printPontos $ cauda cau
+    
+  | otherwise = do -- Normal
+    putStr $ show (cab) ++ " "
+    if cau /= [] then do 
+      putStr $ show (cabeca cau) ++ " "
+      if cauda (cauda cau) /= [] then do
+        putStr "| "
+      else putStr ""
+      printPontos $ cauda cau
+    else putStr ""
 
 main :: IO ()
 main = do
-    putStrLn "Digite o placar de pontos: "
     line <- getLine
     let numbers = map read (words line)
 
-    let pontos = 0
-
-    putStrLn $ "Pontuacao: " ++ show (somaPontos pontos numbers)
-
-
-
-
+    printPontos numbers
+    putStrLn $ "| " ++ show (somaPontos numbers)
 
     
